3bb6994b975a0f7c1c09d9b9acf111642fc92022
unknown <miamby111@gmail.com>
2023-07-16 19:15:27 +0200

    hooks useState , useMemo

---
+++ car-catalog/src/Components/screens/home/Home.js
@@ -0,0 +1,48 @@
+import CarItem from "./car-item/Caritem";
+import { cars as carsData } from "./cars.data.js";
+import CreateCarForm from "./car-item/create-car-form/CreateCarForm.js";
+import { useState } from "react";
+
+function Home() {
+  const [cars, setCars] = useState(carsData);
+  return (
+    <div>
+      <h1>Cars catalog</h1>
+      <CreateCarForm setCars={setCars} />
+      <div>
+        {cars.length ? (
+          cars.map((car) => <CarItem key={car.id} car={car} />)
+        ) : (
+          <p>There are no cars</p>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default Home;
+
+// import { useMemo } from "react";
+// import CarItem from "./car-item/Caritem";
+// import { cars } from "./cars.data.js";
+// function Home() {
+//   const filteredCars = useMemo(
+//     () => cars.filter((car) => car.price > 20000),
+//     []
+//   );
+
+//   return (
+//     <div>
+//       <h1>Cars catalog</h1>
+//       <div>
+//         {filteredCars.length ? (
+//           filteredCars.map((car) => <CarItem key={car.id} car={car} />)
+//         ) : (
+//           <p>There are no cars</p>
+//         )}
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default Home;
---
+++ car-catalog/src/Components/screens/home/cars.data.js
@@ -0,0 +1,23 @@
+export const cars = [
+  {
+    id: 1,
+    name: "Toyota Camry",
+    price: 24970,
+    image:
+      "https://motor.ru/imgs/2023/03/27/07/5850601/b7834cb797d709635d14892ca09f97f395ea4137.jpg",
+  },
+  {
+    id: 2,
+    name: "Honda Civic",
+    price: 22545,
+    image:
+      "https://immagini.alvolante.it/sites/default/files/styles/image_gallery_big/public/news_galleria/2022/06/371366_2022_honda_civic_e_hev.jpg?itok=tEEdCgTY",
+  },
+  {
+    id: 3,
+    name: "Honda Civic 2",
+    price: 15545,
+    image:
+      "https://immagini.alvolante.it/sites/default/files/styles/image_gallery_big/public/news_galleria/2022/06/371366_2022_honda_civic_e_hev.jpg?itok=tEEdCgTY",
+  },
+];
