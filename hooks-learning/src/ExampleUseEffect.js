3eda8bfd054a84ca82a7bb089e666392e74bab08
unknown <miamby111@gmail.com>
2023-07-21 14:39:07 +0200

    useState useEffect example

--- hooks-learning/src/ExampleUseEffect.js
+++ hooks-learning/src/ExampleUseEffect.js
@@ -2,12 +2,32 @@ import React, { useState, useEffect } from "react";
 
 function ExampleUseEffect() {
   const [count, setCount] = useState(0);
-
-  // Аналогично componentDidMount и componentDidUpdate:
   useEffect(() => {
-    // Обновляем заголовок документа с помощью API браузера
+    // 1)Обновляем заголовок документа с помощью API браузера
     document.title = `Вы нажали ${count} раз`;
-  });
+  }); //  при каждом нажатии на кнопку заголовок документа будет изменяться
+
+  useEffect(() => {
+    console.log("Компонент отрендерился!"); // 2) Каждый раз, когда вы будете обновлять состояние count, компонент будет рендериться снова, и функция в useEffect будет вызываться снова, выводя сообщение о рендеринге в консоль.
+
+    // Функция обработчик события, которая будет вызываться при нажатии клавиши
+    const handleKeyPress = (event) => {
+      if (event.key === "Enter") {
+        setCount(count + 1);
+      }
+    };
+
+    // Добавляем слушателя события клавиатуры
+    document.addEventListener("keydown", handleKeyPress);
+
+    // Возвращаем функцию очистки, которая будет вызвана при размонтировании компонента
+    return () => {
+      console.log("Компонент размонтирован!");
+
+      // Удаляем слушателя события клавиатуры при размонтировании компонента
+      document.removeEventListener("keydown", handleKeyPress);
+    };
+  }, [count]); // Передаем count как зависимость
 
   return (
     <div className="wrapper">
--- hooks-learning/src/ExampleUseState.js
+++ hooks-learning/src/ExampleUseState.js
@@ -1,14 +1,24 @@
 import React, { useState } from "react";
 
 function ExampleUseState() {
-  // Объявление переменной состояния, которую мы назовём "count"
   const [count, setCount] = useState(0);
+  const [buttonClicked, setButtonClicked] = useState(false);
+
+  const handleButtonClick = () => {
+    setCount(count + 1);
+    setButtonClicked(true);
+  };
 
   return (
     <div className="wrapper">
       <h1>"ExampleUseState"</h1>
       <p>Вы кликнули {count} раз</p>
-      <button onClick={() => setCount(count + 1)}>Нажми на меня</button>
+      {buttonClicked ? (
+        <p>Кнопка была нажата!</p>
+      ) : (
+        <p>Кнопка еще не была нажата</p>
+      )}
+      <button onClick={handleButtonClick}>Нажми на меня</button>
       <br />
     </div>
   );
